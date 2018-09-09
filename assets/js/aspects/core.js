aaaf543c63a1f8b20c170fffa53a34b97988ec1a
TAYAA Med Amine <tayamino@gmail.com>
2018-09-09 22:01:02 +0100

    Debugging ...

--- assets/js/aspects/core.js
+++ assets/js/aspects/core.js
@@ -1,26 +1,6 @@
 var App = {
-    vues: {},
-    data: {
-        person: {
-        },
-        skills: {
-        },
-        worked: {
-            category: [
-{ name: 'catMotion', title: 'Motion creation' },
-{ name: 'catWeb',    title: 'Web sites' },
-{ name: 'logo',      title: 'Logo design' },
-            ],
-            projects: [
-{ label: 'logo',      title: 'Project #1', cover: 'images/portfolio/1.jpg', cycle: "2008", links: { web: "" } },
-{ label: 'catWeb',    title: 'Project #2', cover: 'images/portfolio/2.jpg', cycle: "2010", links: { web: "" } },
-{ label: 'catMotion', title: 'Project #3', cover: 'images/portfolio/3.jpg', cycle: "2012", links: { web: "" } },
-{ label: 'logo',      title: 'Project #4', cover: 'images/portfolio/4.jpg', cycle: "2014", links: { web: "" } },
-{ label: 'catWeb',    title: 'Project #5', cover: 'images/portfolio/5.jpg', cycle: "2016", links: { web: "" } },
-{ label: 'catMotion', title: 'Project #6', cover: 'images/portfolio/6.jpg', cycle: "2018", links: { web: "" } },
-            ],
-        },
-    },
+    vues: [],
+    data: {},
     hook: function (event, callback) {
         if (typeof(App.objs.events[event])=='undefined') {
             App.objs.events[event] = [];
@@ -90,7 +70,18 @@ var App = {
 
         App.slot('init');
     },
-    load: function ($) {
+    load: function (narrow, $) {
+        var narrow = App.vues;
+
+        App.vues = {};
+
+        for (var i=0 ; i<narrow.length ; i++) {
+            App.vues[narrow[i]] = new Vue({
+                el: '#'+narrow[i],
+                data: App.data[narrow[i]],
+            });
+        }
+
         App.slot('load', [$]);
     },
 };
