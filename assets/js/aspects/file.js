982759add64da985c232215f7ef16d2623d3a938
TAYAA Med Amine <tayamino@gmail.com>
2018-09-09 22:11:13 +0100

    Debugging ...

--- assets/js/aspects/file.js
+++ assets/js/aspects/file.js
@@ -1,7 +1,28 @@
 Site.skillbar = {
-    objs: null,
     init: function() {
-        
+        App.data.vues.push('resume');
+
+        App.data.resume = {
+            skills: [
+                { title: "Web skills", group: [
+{ title: 'PHP 7.0+', metrise: '100%' },
+{ title: 'Wordpress', metrise: '95%' },
+{ title: 'Django', metrise: '65%' },
+{ title: 'Laravel', metrise: '35%' },
+                ] },
+                { title: "Programming skills", group: [
+{ title: 'PHP 7.0+', metrise: '100%' },
+{ title: 'Wordpress', metrise: '95%' },
+{ title: 'Django', metrise: '65%' },
+{ title: 'Laravel', metrise: '35%' },
+                ] },
+            ],
+            projects: [
+{ label: 'logo',      title: 'Project #1', cover: 'images/portfolio/1.jpg', cycle: "2008", links: { web: "" } },
+{ label: 'catWeb',    title: 'Project #2', cover: 'images/portfolio/2.jpg', cycle: "2010", links: { web: "" } },
+{ label: 'catMotion', title: 'Project #3', cover: 'images/portfolio/3.jpg', cycle: "2012", links: { web: "" } },
+            ],
+        };
     },
     load: function($) {
         $('.tabs-resume').click(function() {
