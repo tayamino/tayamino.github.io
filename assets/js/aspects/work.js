7ea880aea1d4b1cd40b069293a0a816719f409a5
TAYAA Med Amine <tayamino@gmail.com>
2018-09-09 21:36:50 +0100

    Debugging ...

--- assets/js/aspects/work.js
+++ assets/js/aspects/work.js
@@ -1,6 +1,23 @@
 Site.portfolio = {
     init: function() {
-        
+        App.vues.portfolio = new Vue({
+            el: '#portfolio',
+            data: {
+                category: [
+{ name: 'catMotion', title: 'Motion creation' },
+{ name: 'catWeb',    title: 'Web sites' },
+{ name: 'logo',      title: 'Logo design' },
+                ],
+                projects: [
+{ label: 'logo',      title: 'Project #1', cover: 'images/portfolio/1.jpg', cycle: "2008" },
+{ label: 'catWeb',    title: 'Project #2', cover: 'images/portfolio/2.jpg', cycle: "2010" },
+{ label: 'catMotion', title: 'Project #3', cover: 'images/portfolio/3.jpg', cycle: "2012" },
+{ label: 'logo',      title: 'Project #4', cover: 'images/portfolio/4.jpg', cycle: "2014" },
+{ label: 'catWeb',    title: 'Project #5', cover: 'images/portfolio/5.jpg', cycle: "2016" },
+{ label: 'catMotion', title: 'Project #6', cover: 'images/portfolio/6.jpg', cycle: "2018" },
+                ],
+            },
+        });
     },
     load: function($) {
         // MixItUp plugin
