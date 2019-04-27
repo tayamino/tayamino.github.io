63c787ab806052a5f400ff182e3ad64d7cfc6e2b
TAYAA Med Amine <amine@tayaa.me>
2019-04-27 14:17:29 +0100

    Updating references ...

--- assets/js/aspects/main.js
+++ assets/js/aspects/main.js
@@ -137,15 +137,17 @@ App.hook('load', function($) {
     });
 }, "redimensionnement");
 
+/*
 App.hook('load', function($) {
     $("a[rel^='portfolio']").prettyPhoto({
-        animation_speed: 'fast', /* fast/slow/normal */
+        animation_speed: 'fast', // fast/slow/normal
         social_tools: '',
         theme: 'pp_default',
         horizontal_padding: 5,
         deeplinking: false,
     });
 }, "prettyPhoto");
+//*/
 
 /*
 App.hook('load', function($) {
